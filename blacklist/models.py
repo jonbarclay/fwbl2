0aca65c3709b8875d7991ef4a81c6fb929b608b4
jonbarclay <jon.barclay@gmail.com>
2017-03-20 16:55:40 -0600

    ip_address added

--- blacklist/models.py
+++ blacklist/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
-##
-
-##
+class ip_address(models.Model):
+    ip = models.CharField(max_length=200)
+    comment = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
